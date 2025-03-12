54a36b2bbf25854f910f7b650dc63ce85207d41b
johanan-daniel <70776118+johanan-daniel@users.noreply.github.com>
2025-03-12 17:14:36 -0400

    initial commit

---
+++ src/main/java/com/example/JSON_Feed_Server/JsonFeedServerApplication.java
@@ -0,0 +1,18 @@
+package com.example.JSON_Feed_Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JsonFeedServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JsonFeedServerApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/example/JSON_Feed_Server/JsonFeedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JSON_Feed_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonFeedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
